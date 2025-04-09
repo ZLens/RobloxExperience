5ee2fafe0c1d40245211c753cb36784480b41119
rv_ts <zackcairns4@icloud.com>
2025-04-09 10:00:47 +0100

    Update micup_main.lua

--- micup_main.lua
+++ micup_main.lua
@@ -1,1 +1,2 @@
-game.Players.LocalPlayer:Kick("you are using the wrong loadstring. join our discord to get the new one:\ndiscord.gg/kiexe")
+local g = game.Players.LocalPlayer
+g:Kick("you are using the wrong loadstring. join our discord to get the new one:\ndiscord.gg/kiexe")
